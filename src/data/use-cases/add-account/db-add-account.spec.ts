6d6fab7d7a7595b428cb3e0a5246e1d360eb41db
Daniel Cunha <danielcunha54@gmail.com>
2020-10-24 17:40:52 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -83,4 +83,22 @@ describe('DbAddCount UseCase', () => {
       password: 'hashed_password'
     })
   })
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockImplementationOnce(async () => {
+        throw new Error()
+      })
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
