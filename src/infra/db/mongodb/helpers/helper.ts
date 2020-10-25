6e251b8e3bad16cefb942191ac73e7652d303328
Daniel Cunha <danielcunha54@gmail.com>
2020-10-25 07:51:59 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/helper.ts
+++ src/infra/db/mongodb/helpers/helper.ts
@@ -17,6 +17,14 @@ class _MongoHelper {
   getCollection(name: string): Collection {
     return this.client?.db().collection(name)
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  map<TData = any>({ _id: id, ...entity }: any): TData {
+    return {
+      id,
+      ...entity
+    }
+  }
 }
 
 export const MongoHelper = new _MongoHelper()
