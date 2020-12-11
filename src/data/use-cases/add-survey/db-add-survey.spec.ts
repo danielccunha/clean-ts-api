e541a88f198115739647b289a70fd2993b4e97c6
Daniel Cunha <danielcunha54@gmail.com>
2020-12-11 10:46:33 -0300

    test: ensure DbAddSurvey calls AddSurveyRepository with correct values

---
+++ src/data/use-cases/add-survey/db-add-survey.spec.ts
@@ -0,0 +1,32 @@
+import { AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
+import { DbAddSurvey } from './db-add-survey'
+
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    }
+  ]
+})
+
+const makeAddSurveyRepository = (): AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    async add(): Promise<void> {
+      //
+    }
+  }
+  return new AddSurveyRepositoryStub()
+}
+
+describe('DbAddSurvey UseCase', () => {
+  test('Should call AddSurveyRepository with correct values', async () => {
+    const addSurveyRepositoryStub = makeAddSurveyRepository()
+    const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
+    const sut = new DbAddSurvey(addSurveyRepositoryStub)
+    const surveyData = makeFakeSurveyData()
+    await sut.add(surveyData)
+    expect(addSpy).toHaveBeenCalledWith(surveyData)
+  })
+})
